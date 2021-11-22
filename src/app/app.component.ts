b4e39182cbc14cdcbfafe052e7266f211fd9ca20
Panchaya-M <panchaya.meeboonpor@gmail.com>
2021-11-22 20:37:09 +0700

    Add Fizz and Buzz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Fizz-Buzz';
+  number = 0
+  array = Array()
+
+  fizzBuzz() {
+    for (let i = 1; i <= this.number; i++) {
+      if (i % 3 == 0 && i % 5 == 0) { this.array.push('FizzBuzz') }
+      else if (i % 3 == 0) { this.array.push('Fizz') }
+      else if (i % 5 == 0) { this.array.push('Buzz') }
+      else { this.array.push(''+i) }
+    }
+    this.number
+  }
+
 }
